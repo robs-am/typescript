8ae341a2351c345fc980e31075158d68b707fbf6
Roberta <roberta.amaro@ed3.com.br>
2023-10-12 14:53:46 -0300

    type annotation

---
+++ types/script.js
@@ -0,0 +1,10 @@
+//@ts-check
+const frase = "Front End";
+const total = 100.05;
+const empresas = ["Apple", "Microsoft"];
+
+console.log(frase.toLowerCase());
+
+console.log(total.toFixed());
+
+console.log(empresas.map((empresa) => empresa.toLowerCase()));
